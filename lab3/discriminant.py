79fa8536da180f568a0194412e034d5ba15493d1
Orlova_Yaroslava <orlovayaroslawa@yandex.ru>
2025-10-13 14:47:25 +0700

    дополнен пункт 1.5  3лр

--- lab3/discriminant.py
+++ lab3/discriminant.py
@@ -11,4 +11,3 @@ def calculate_discriminant(a, b, c):
     if not all(isinstance(x, (int, float)) for x in (a, b, c)):
         raise TypeError("Все аргументы должны быть числами (int или float).")
     return float(b * b - 4 * a * c)
-
